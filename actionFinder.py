d0b3d3e75804ecb7b62b879a954bbf4744cc85ff
unigames <44467022+unigames@users.noreply.github.com>
2018-10-25 19:35:37 +0800

    Add actionFinder.py
    Add actionFinder.py

---
+++ actionFinder.py
@@ -0,0 +1,43 @@
+import sys
+import re
+
+pattern = re.compile("\t*>(.+?):\s(.+)")
+
+committeeNames = ['Gavin','Aoibhinn','Alaura','Donald','Nadia','Jasmine','Alistair','Lewis','Jake','Taylor','Other']
+committeeActions = {'Other':[]}
+for member in committeeNames:
+    committeeActions[member] = []
+
+
+args = sys.argv
+if len(args) != 2:
+    print("Usage: actionFinder.py [filename]")
+else:
+    minutes = open(args[1], "r")
+    matches = pattern.findall(minutes.read())
+    for match in matches:
+        action = match[1].strip()
+        if committeeActions.has_key(match[0]):
+            committeeActions[match[0]].append(action)
+        else:
+            involved = []
+            for name in committeeActions.keys():
+                if name in match[0]:
+                    involved.append(name)
+            if len(involved) == 1:
+                committeeActions[involved[0]].append(action)
+            if len(involved) == 0:
+                committeeActions['Other'].append(match[0]+": "+action)
+            else:
+                for name in involved:
+                    committeeActions[name].append(action+' (along with '+(', '.join([n for n in involved if n!=name]))+')')
+    #print committeeActions
+    # Print results
+    for name in committeeNames:
+    	if len(committeeActions[name]) == 0:
+    		continue
+        print(name+":")
+        for item in committeeActions[name]:
+            print("> "+item[:1].upper()+item[1:])
+        print("")
+        
